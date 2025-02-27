d970d8b9de724853241616cbb497e07ae91343d0
jinchuan <1209269330@qq.com>
2025-02-27 15:43:43 +0800

    项目搭建

---
+++ mall-coupon/src/test/java/com/yjc/jcmall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjc.jcmall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/com/yjc/jcmall/mallware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjc.jcmall.mallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
